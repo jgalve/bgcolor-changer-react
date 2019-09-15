6beebd61b9630f6d0fc8119706187bab08e7c4df
jgalve <jonathangalve@yahoo.com>
2019-09-15 18:22:18 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Random from './Random';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Random />
+    </div>
+  );
+}
+
+export default App;
